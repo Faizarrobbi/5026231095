809a37a37ab7329e273834c6cd3222fa167d97fd
Akhtar Faizarrobbi <5026231095@student.its.ac.id>
2025-06-03 19:37:37 +0700

    CRUD 2

--- app/Http/Controllers/PegawaiDBController.php
+++ app/Http/Controllers/PegawaiDBController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\DB;
 
 class PegawaiDBController extends Controller
 {
+    // Method untuk membaca database
     public function index()
     {
     	// mengambil data dari table pegawai
@@ -17,25 +18,45 @@ class PegawaiDBController extends Controller
 
     }
 
+    // Method untuk menambahkan ke database
     public function tambah()
     {
-
-	// memanggil view tambah
-	return view('tambah');
-
+	    // memanggil view tambah
+	    return view('tambah');
     }
-
     public function store(Request $request)
     {
-	// insert data ke table pegawai
-	DB::table('pegawai')->insert([
-		'pegawai_nama' => $request->nama,
-		'pegawai_jabatan' => $request->jabatan,
-		'pegawai_umur' => $request->umur,
-		'pegawai_alamat' => $request->alamat
-	]);
-	// alihkan halaman ke halaman pegawai
-	return redirect('/pegawai');
+	    // insert data ke table pegawai
+	    DB::table('pegawai')->insert([
+		    'pegawai_nama' => $request->nama,
+		    'pegawai_jabatan' => $request->jabatan,
+		    'pegawai_umur' => $request->umur,
+		    'pegawai_alamat' => $request->alamat
+	    ]);
+	    // alihkan halaman ke halaman pegawai
+	    return redirect('/pegawai');
+    }
 
+    // Method untuk edit data pegawai
+    public function edit($id)
+    {
+	    // mengambil data pegawai berdasarkan id yang dipilih
+	    $pegawai = DB::table('pegawai')->where('pegawai_id',$id)->get();
+	    // passing data pegawai yang didapat ke view edit.blade.php
+	    return view('edit',['pegawai' => $pegawai]);
+    }
+
+    // Method untuk update data pegawai
+    public function update(Request $request)
+    {
+	    // update data pegawai
+	    DB::table('pegawai')->where('pegawai_id',$request->id)->update([
+		    'pegawai_nama' => $request->nama,
+		    'pegawai_jabatan' => $request->jabatan,
+		    'pegawai_umur' => $request->umur,
+		    'pegawai_alamat' => $request->alamat
+	    ]);
+	    // alihkan halaman ke halaman pegawai
+	    return redirect('/pegawai');
     }
 }
